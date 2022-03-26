07df496805bc1102bab070f0fa950377dfc1722e
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-26 18:01:53 +0700

    preload fonts using link in index.html to prevent FOIT

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -11,34 +11,6 @@ const CustomStyles = createGlobalStyle`
       text-main-text
     `}
   }
-
-  @font-face {
-      font-family: 'Kollektif';
-      src: url('/fonts/Kollektif/Kollektif.woff2') format('woff2'),
-          url('/fonts/Kollektif/Kollektif.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-      font-display: swap;
-  }
-
-  @font-face {
-      font-family: 'Kollektif Bold';
-      src: url('/fonts/Kollektif/Kollektif-Bold.woff2') format('woff2'),
-          url('/fonts/Kollektif/Kollektif-Bold.woff') format('woff');
-      font-weight: bold;
-      font-style: normal;
-      font-display: swap;
-  }
-
-  @font-face {
-      font-family: 'Norwester';
-      src: url('/fonts/Norwester/Norwester-Regular.woff2') format('woff2'),
-          url('/fonts/Norwester/Norwester-Regular.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-      font-display: swap;
-  }
-
 `;
 
 const GlobalStyles = () => (
