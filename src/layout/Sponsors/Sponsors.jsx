86caddbd8423f2f7d831570570ea3f6ccf93f5ab
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-27 16:43:52 +0700

    add sponsors section

--- src/layout/Sponsors/Sponsors.jsx
+++ src/layout/Sponsors/Sponsors.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
-import { SponsorsWrapper } from './Sponsors.style';
+import { ReactSVG } from 'react-svg';
+import Marquee from 'react-fast-marquee';
+import {
+  SponsorsWrapper,
+  SponsorList,
+  SponsorItem,
+} from './Sponsors.style';
+import SectionHeader from '../../common/SectionHeader/SectionHeader';
+
+let sponsorList = Array.from(Array(12).keys());
+sponsorList = sponsorList.slice(sponsorList.indexOf(1));
 
 const Sponsors = () => {
   return (
     <SponsorsWrapper id="sponsors">
-      Sponsors
+      <SectionHeader
+        title="Pellentesque finibus, dui et pretium venenatis"
+        subtitle="Sponsors"
+      />
+      <Marquee
+        speed={50}
+        gradient={false}
+      >
+        <SponsorList>
+          {
+            sponsorList.map((item) => {
+              return (
+                <SponsorItem
+                  key={`/images/sponsors/${item}.svg`}
+                >
+                  <ReactSVG
+                    src={`/images/sponsors/${item}.svg`}
+                  />
+                </SponsorItem>
+              );
+            })
+          }
+        </SponsorList>
+      </Marquee>
     </SponsorsWrapper>
   );
 };
--- src/layout/Sponsors/Sponsors.style.jsx
+++ src/layout/Sponsors/Sponsors.style.jsx
@@ -3,6 +3,57 @@ import { Wrapper } from '../../common/Wrapper';
 
 export const SponsorsWrapper = styled(Wrapper)`
   ${tw`
-    flex
+    flex 
+    flex-col 
   `}
+
+  .marquee-container{
+    ${tw`
+      pt-5
+      pb-10
+    `}
+  }
+`;
+
+export const SponsorList = styled.div`
+  ${tw`
+    w-full 
+    flex  
+    px-5
+    gap-x-8
+    md:gap-x-20 
+  `}
+`;
+
+export const SponsorItem = styled.div`
+  ${tw`
+    rounded-md 
+    py-4
+    px-5
+    md:px-6
+  `}
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+  
+  & > div{
+    ${tw`
+      flex 
+      items-center 
+    `}
+  }
+
+
+  & > div, svg {
+    ${tw`
+      w-[120px]
+      h-[40px]
+      md:w-[200px]
+      md:h-[70px]
+    `}
+  }
+
+  svg path, svg text{
+    ${tw`
+      fill-current text-accent
+    `}
+  }
 `;
