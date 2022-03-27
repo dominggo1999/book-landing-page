9415178553da63197092bd42417b34f9a075b5de
dominggo1999 <arnolddominggo19@gmail.com>
2022-03-27 18:09:00 +0700

    chapters

--- src/layout/Chapters/Chapters.jsx
+++ src/layout/Chapters/Chapters.jsx
@@ -1,10 +1,105 @@
 import React from 'react';
-import { ChapterWrapper } from './Chapters.style';
+import {
+  ChapterWrapper,
+  ChapterList,
+  ChapterItem,
+  ChapterNumber,
+  ChapterContent,
+  ChapterTitle,
+  ChapterDescription,
+} from './Chapters.style';
+import SectionHeader from '../../common/SectionHeader/SectionHeader';
 
 const Chapters = () => {
   return (
     <ChapterWrapper id="chapters">
-      Chapters
+      <SectionHeader
+        subtitle="Chapters"
+        title="Praesent fermentum cursus risus a sodales"
+      />
+      <ChapterList>
+        <ChapterItem>
+          <ChapterNumber>1</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>2</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>3</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>4</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>5</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>6</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>6</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>6</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+        <ChapterItem>
+          <ChapterNumber>6</ChapterNumber>
+          <ChapterContent>
+            <ChapterTitle>Lorem, ipsum dolor.</ChapterTitle>
+            <ChapterDescription>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quod fugiat incidunt eaque blanditiis consequuntur totam!
+            </ChapterDescription>
+          </ChapterContent>
+        </ChapterItem>
+      </ChapterList>
     </ChapterWrapper>
   );
 };
--- src/layout/Chapters/Chapters.style.jsx
+++ src/layout/Chapters/Chapters.style.jsx
@@ -3,6 +3,67 @@ import { Wrapper } from '../../common/Wrapper';
 
 export const ChapterWrapper = styled(Wrapper)`
   ${tw`
-    
+    py-20
+    flex 
+    flex-col 
+    gap-y-5
+  `}
+`;
+
+export const ChapterList = styled.div`
+  ${tw`
+    grid
+    grid-cols-1 
+    md:grid-cols-2 
+    lg:grid-cols-3 
+    gap-x-2
+    xl:gap-x-10
+    gap-y-10
+  `}
+`;
+
+export const ChapterItem = styled.div`
+  ${tw`
+    rounded-lg 
+    flex 
+  `}
+`;
+
+export const ChapterNumber = styled.div`
+  ${tw`
+    text-2xl 
+    font-secondary 
+    w-[2.75rem]
+    h-[2.75rem] 
+    rounded-full 
+    border
+    border-accent
+    text-accent 
+    flex 
+    items-center 
+    justify-center
+    mr-4
+  `}
+
+  aspect-ratio : 1 / 1;
+`;
+
+export const ChapterContent = styled.div`
+  ${tw`
+    flex 
+    flex-col
+  `}
+`;
+
+export const ChapterTitle = styled.div`
+  ${tw`
+    font-secondary 
+    text-2xl
+  `}
+`;
+
+export const ChapterDescription = styled.div`
+  ${tw`
+    text-lg
   `}
 `;
